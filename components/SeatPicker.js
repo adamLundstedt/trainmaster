a154491e02d6170b545cb0a728d5460fb3975fa4
Antonio Gallardo Girela <gallardogirela@gmail.com>
2022-09-22 17:19:06 +0200

    creating components

--- components/SeatPicker.js
+++ components/SeatPicker.js
@@ -1,23 +1,30 @@
 import { mongo } from "mongoose";
 import { connectToDatabase } from "../lib/mongodb";
 
-export default function SeatPicker(trains, coaches, bookings, tickets) {
+export default function SeatPicker(trains, coaches, bookings, tickets, routes) {
+  
+  const chosenTrainId = '632ac4233b9bdbfc822b4d13';    
 
     
-    return <h1>Seat Picker</h1>
+    return (
+      <div></div>
+    );
 
     
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(chosenTrainId) {
     const { db } = await connectToDatabase();  
-    mongo.s;  
+    mongo.s;
+    const chosenTrain = await db.collection("trains").find({_id : chosenTrainId}).toArray(); 
+    const routes = await db.collection("routes").find({}).toArray(); 
     const trains = await db.collection("trains").find({}).toArray();
     const coaches = await db.collection("coaches").find({}).toArray();
     const bookings = await db.collection("bookings").find({}).toArray();
-    const tickets = await db.collection("tickets").find({}).toArray();
+    const tickets = await db.collection("tickets").find({_id : chosenTrainId}).toArray();
     return {
       props: {
+        routes: JSON.parse(JSON.stringify(routes)),
         trains: JSON.parse(JSON.stringify(trains)),
         coaches: JSON.parse(JSON.stringify(coaches)),
         bookings:  JSON.parse(JSON.stringify(bookings)),
