593d9a5a3a6abdeb65565d9f9e47f31396df3a05
Michele <vilda1002@gmail.com>
2022-09-22 10:34:04 +0200

    ticketModel

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,1 +1,31 @@
-empty DB
+import mongoose from "mongoose";
+
+const ticket = new mongoose.Schema({
+  ticketId: {
+    type: ObjectId,
+  },
+  trainId: {
+    type: ObjectId,
+  },
+  coach: {
+    type: ObjectId,
+  },
+  startStation: {
+    type: String,
+  },
+  endStation: {
+    type: String,
+  },
+  bookingId: {
+    type: ObjectId,
+  },
+  price: {
+    type: Number,
+  },
+  seatNumber: {
+    type: Number,
+  },
+  route: {
+    type: ObjectId,
+  },
+});
