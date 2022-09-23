a636348c4e5abecf6381baa1797577a0671b193a
Michele <vilda1002@gmail.com>
2022-09-23 09:23:38 +0200

    test top

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
-
-
 export default function Home() {
-  return <h1>Hej Tail</h1>
+  return (
+    <div>
+      <h1 className="text-blue-400">Registrera medlem</h1>
+      <form>
+        <label className="text-blue-400">Förnamn</label>
+        <input type="text" placeholder="Skriv in ditt förnamn" />
+        <label>Efternamn</label>
+        <input type="text" placeholder="Skriv in ditt efternamn" />
+        <label>Email</label>
+        <input type="text" name="email" placeholder="Skriv in din email" />
+        <label>Telefonnummer</label>
+        <input type="text" placeholder="Skriv in ditt telefonnummer" />
+        <label>Användarnamn</label>
+        <input type="text" name="userName" placeholder="användarnamn" />
+        <label>Lösenord</label>
+        <input
+          type="text"
+          name="password"
+          placeholder="minst 8 bokstäver eller siffror "
+        />
+        <input type="submit" value="Registrera" />
+      </form>
+    </div>
+  );
 }
