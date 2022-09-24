4256ac793957c1c5d05c8de7c7f0f0ae4f9db161
Adam Lundstedt <adamlundstedt@rocketmail.com>
2022-09-24 08:33:14 +0200

    added 404 catch

---
+++ components/SearchTrip.js
@@ -0,0 +1,15 @@
+import { mongo } from "mongoose";
+import { connectToDatabase } from "../lib/mongodb";
+
+export default function SearchTrip() {
+  
+  
+      
+
+    
+    return (
+      <div></div>
+    );
+
+    
+}
