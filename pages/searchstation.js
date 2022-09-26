fe0c61d11e99650d649c7b122c6a1dc9f7424341
Adam Lundstedt <adamlundstedt@rocketmail.com>
2022-09-26 18:41:45 +0200

    work in progress

--- pages/searchstation.js
+++ pages/searchstation.js
@@ -1,12 +1,38 @@
 import { mongo } from "mongoose";
+import { useState } from "react";
 import { connectToDatabase } from "../lib/mongodb";
 
-export async function getStaticProps () {
+
+
+export default function SearchStation({ routes }) {
+
+  let stationsNames = [];
+
+  for(let stationName of routes.stations)
+  const [stationsName, setStationsName] = useState(routes);
+
+
+  return (
+    <div>
+      
+      {routes.map((route) => (
+          <li key={route._id}>
+            <h2>{route.}</h2>
+            
+          </li>
+        ))}
+      
+    </div>
+  );
+}
+ 
+export async function getServerSideProps(context) {
   
   const { db } = await connectToDatabase();
   mongo.s;
 
   const routes = await db.collection("routes").find({}).limit(5).toArray();
+  const stations = await db.collection("stations").find({}).toArray();
   
  
   return {
@@ -18,23 +44,6 @@ export async function getStaticProps () {
 
   
 }
-
-
-export default function SearchStation({ routes }) {
-  return (
-    <div>
-      
-      {routes.map((route) => (
-          <li key={route._id}>
-            <h2>{route.routeName}</h2>
-            
-          </li>
-        ))}
-      
-    </div>
-  );
-}
- 
 
 
 /* export async function getStaticProps() {
@@ -50,4 +59,28 @@ export default function SearchStation({ routes }) {
       users: JSON.parse(JSON.stringify(users)),
     },
   };
-} */
+} 
+
+export async function getServerSideProps(context) {    
+   
+  const { db } = await connectToDatabase();    
+  
+  const trainsData = await db.collection("trains").find({}).toArray();
+  const ticketsData = await db.collection("tickets").find({}).toArray();
+  const coachesData = await db.collection("coaches").find({}).toArray();
+    
+    
+  const trains = JSON.parse(JSON.stringify(trainsData));
+  const tickets = JSON.parse(JSON.stringify(ticketsData));
+  const coaches = JSON.parse(JSON.stringify(coachesData));
+   
+
+  return {
+    props: {        
+      trains : trains,
+      tickets : tickets,
+      coaches : coaches,        
+    },
+  };
+}*/
+
