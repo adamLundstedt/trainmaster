f845b99dad18b35a7ea9f0e626933382f0caa49d
Adam Lundstedt <adamlundstedt@rocketmail.com>
2022-09-29 13:42:10 +0200

    save commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="h-full ">
       <div className="pt-[100px] pb-[450px] bg-[url('/background.jpeg')] bg-cover bg-center  bg-no-repeat">
         <a className="text-white font-bold text-[25px] m-20 md:text-[80px] ">Vart vill du resa?</a>
+        <Link href="/searchTrip">
         <div className="w-[200px] ml-20 bg-gray-400 cursor-pointer text-center drop-shadow-md shadow-black text-white rounded text-sm">
           Sök resa
-        </div>
+        </div></Link>
       </div>
     </div>
   )
--- pages/searchTrip.js
+++ pages/searchTrip.js
@@ -12,6 +12,31 @@ const typeOfTicket = [
   { id: 5, name: 'Senior' },
 ]
 
+const station = [
+  { id: 1, name: 'Till' },
+  //{ id: 1, name: 'Stockholm C' },
+  { id: 2, name: 'Sundbyberg' },
+  { id: 3, name: 'Bålsta' },
+  { id: 5, name: 'Enköping' },
+  { id: 6, name: 'Västerås C' },
+  { id: 7, name: 'Köping' },
+  { id: 8, name: 'Arboga' },
+  { id: 9, name: 'Örebro C' },
+  { id: 10, name: 'Örebro Södra' },
+  { id: 11, name: 'Kumla' },
+  { id: 12, name: 'Hallsberg' },
+  { id: 13, name: 'Laxå' },
+  { id: 14, name: 'Töreboda' },
+  { id: 15, name: 'Skövde C' },
+  { id: 16, name: 'Falköping C' },
+  { id: 17, name: 'Herrljunga' },
+  { id: 18, name: 'Vårgårda' },
+  { id: 19, name: 'Alingsås' },
+  { id: 20, name: 'Göteborg C' },
+  { id: 21, name: 'Mölndal' },
+  //{ id: , name: '' },
+]
+
 export default function Home() {
   return (
     <div className="h-full ">
@@ -20,7 +45,8 @@ export default function Home() {
 
         <div className="mt-8 grid grid-cols-2 items-center w-full">
           <div className="w-[100px] ml-10 bg-gray-400 cursor-pointer text-center drop-shadow-md shadow-black text-white rounded text-sm">
-            Till        </div>
+            <Stations />        </div>
+          
           <div className="w-[100px] ml-10 bg-gray-400 cursor-pointer text-center drop-shadow-md shadow-black text-white rounded text-sm">
             Från        </div>
         </div>
@@ -98,3 +124,62 @@ function MyListBox() {
     </div>
   )
 }
+
+function Stations() {
+  const [selected, setSelected] = useState(station[0])
+
+  return (
+    <div className="ml-4 bg-gray-400 rounded text-white mr-5">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-grey  pr-10 text-center shadow-md focus:outline-none   focus-visible:ring-offset-slate-300 ">
+            <span className="block truncate">{selected.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-white"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+
+          <Transition
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-gray-400 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {station.map((type, typeIdx) => (
+                <Listbox.Option
+                  key={typeIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none  pl-8 pr-4 ${active ? 'bg-gray-600 text-white' : 'text-white'
+                    }`
+                  }
+                  value={type}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                          }`}
+                      >
+                        {type.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-white">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  )
+}
+
