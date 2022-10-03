5b4ec8c9d5c6bec3a104038d4651b6d87e4c70f5
Jennie Werther Johansson <denjennie@live.se>
2022-10-03 16:05:20 +0200

    Exit knapp, fix i layout samt datepicker

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { useState } from 'react'
 
-export default function mobileNavbar() {
+
+export default function MobileNavbar() {
+  const [dropdownOpen, setdropdownOpen] = useState(false);
+
 
   return (
     <nav className="bg-white z-50 top-0 flex drop-shadow-md shadow-black">
@@ -12,16 +16,16 @@ export default function mobileNavbar() {
         drop-shadow-md shadow-black text-white rounded text-sm">
           Sök bokning
         </div>
-        <button className="m-3 ml-24 justify-end items-end">
+        <button onClick={() => setdropdownOpen(!dropdownOpen)} className="m-3 ml-24 justify-end items-end"
+        >
           <svg xmlns="http://www.w3.org/2000/svg"
             width="22"
             height="22"
             viewBox="0 0 24 24"
             fill="none"
             stroke="#000000"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round">
+            strokeWidth="2"
+          >
             <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
         </button>
 
