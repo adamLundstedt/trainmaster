3fd50907f6798436c243acbd927a87f1554f693a
Michele <vilda1002@gmail.com>
2022-10-14 11:13:06 +0200

    added minLength and swedish

--- models/Member.js
+++ models/Member.js
@@ -8,8 +8,9 @@ const MemberSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: [true, "Please provide a password for this member!"],
-    maxlength: [30, "Password cannot be more than 20 characters!"],
+    required: [true, "Var god välj ett lösenord!"],
+    maxlength: [30, "Lösenordet kan inte vara längre än 30 tecken långt!"],
+    minLength: [6, "Lösenordet måste vara minst 6 tecken långt"],
   },
 });
 export default mongoose.models.Member || mongoose.model("Member", MemberSchema);
