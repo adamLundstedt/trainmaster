8c989ba6b59294af86871668e8aec44ca237b968
Adam Lundstedt <adamlundstedt@rocketmail.com>
2022-10-19 12:50:51 +0200

    404

---
+++ pages/404.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
+
+  return (
+    <div>
+      <h1>Oooops...</h1>
+      <h2>Något gick fel...</h2>
+      <p>
+        Gå tillbaka till{" "}
+        <Link href="/">
+          <a>Startsidan.</a>
+        </Link>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
