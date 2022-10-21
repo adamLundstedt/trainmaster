2af3f78cad81447b615b203a9cbb99c2986c0a1a
Jennie Werther Johansson <denjennie@live.se>
2022-10-21 13:20:21 +0200

    lagt till en Modal

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 
+
 export default function Home() {
+
   return (
     <div className="h-screen w-full ">
       <Navbar />
@@ -13,6 +15,7 @@ export default function Home() {
           Sök resa
         </div>
       </Link>
-    </div>
+
+    </div >
   );
 }
